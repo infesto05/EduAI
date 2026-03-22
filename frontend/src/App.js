9ac88d25243d4aa303ac4a3a6633f20257a7370b
infesto05 <aasifmohamed32@gmail.com>
2026-03-22 13:48:35 +0530

    fix temp bckend & frntendd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
     setChat(updatedChat);
     setLoading(true);
 
-    const res = await fetch("https://eduai-44we.onrender.com", {
+    const res = await fetch("https://eduai-44we.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ messages: updatedChat }),
